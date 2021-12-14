ea5816b2adcd906f344f8a3c1c0e79baadb03f4a
Shweta Pandey <shwetapandey@iisc.ac.in>
2021-12-14 14:06:38 +0530

    Remove files if present

--- Figure_1/Figure_1b/srad/run.sh
+++ Figure_1/Figure_1b/srad/run.sh
@@ -4,6 +4,13 @@ file=$1
 srad_cpu_file="srad_cpu"
 echo $0 $1
 if [[ "$file" == "$srad_cpu_file" ]]; then
+    rm -f /pmem/persist_e_c.dat
+    rm -f /pmem/persist_n_c.dat
+    rm -f /pmem/persist_s_c.dat
+    rm -f /pmem/persist_w_c.dat
+    rm -f /pmem/persist_j.dat
+    rm -f /pmem/persist_j_out.dat
+    rm -f /pmem/persist_c.dat
     ./bin/$file 131072 1024 0 31 0 31 0.5 50 32;
 else
     ./bin/$file 131072 1024 0 31 0 31 0.5 50;
